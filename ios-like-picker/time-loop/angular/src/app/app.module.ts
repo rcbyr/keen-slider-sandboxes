da86386993250630adb0eef0fe2394695e7e482a
Eric Beyer <contact@ericbeyer.de>
2020-11-08 20:46:10 +0100

    fix angular sandboxes

--- ios-like-picker/time-loop/angular/src/app/app.module.ts
+++ ios-like-picker/time-loop/angular/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 
 import { AppComponent } from "./app.component"
+import { WheelComponent } from "./wheel.component"
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WheelComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
