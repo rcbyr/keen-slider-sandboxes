f7570d40e8e5ea204deed03e447c320315b43d0a
Eric Beyer <contact@ericbeyer.de>
2021-11-26 14:30:13 +0100

    add images

--- media/zoom-out/react-typescript/src/App.tsx
+++ media/zoom-out/react-typescript/src/App.tsx
@@ -3,6 +3,13 @@ import "./styles.css"
 import { useKeenSlider, TrackDetails } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 
+const images = [
+  "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+]
+
 export default function App() {
   const [details, setDetails] = React.useState<TrackDetails | null>(null)
   const [size, setSize] = React.useState<number>(0)
--- media/zoom-out/react/src/App.js
+++ media/zoom-out/react/src/App.js
@@ -3,6 +3,13 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./styles.css"
 
+const images = [
+  "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+]
+
 export default () => {
   const [details, setDetails] = React.useState(null)
   const [size, setSize] = React.useState(0)
