7d2a2983b94a84b559cc6aeccedc6ac6850f53cf
Eric Beyer <contact@ericbeyer.de>
2021-11-26 14:16:16 +0100

    add react

--- navigation/arrows-and-dots/react-typescript/src/App.tsx
+++ navigation/arrows-and-dots/react-typescript/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import "./styles.css"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
