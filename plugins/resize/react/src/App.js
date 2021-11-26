07b75b6aa3a1800f7845977fb2c03879546b42a9
Eric Beyer <contact@ericbeyer.de>
2021-11-27 00:14:05 +0100

    wip

--- plugins/resize/react/src/App.js
+++ plugins/resize/react/src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./styles.css"
