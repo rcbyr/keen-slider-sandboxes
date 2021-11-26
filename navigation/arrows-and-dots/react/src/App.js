0b5df0c9e1e0b5a4ac3a467e1cacfb01bfdff3fc
Eric Beyer <contact@ericbeyer.de>
2021-11-26 14:15:47 +0100

    move styles

--- navigation/arrows-and-dots/react/src/App.js
+++ navigation/arrows-and-dots/react/src/App.js
@@ -18,36 +18,6 @@ export default () => {
 
   return (
     <>
-      <style jsx>{`
-        .navigation-wrapper {
-          position: relative;
-        }
-
-        .dots {
-          display: flex;
-          padding: 10px 0;
-          justify-content: center;
-        }
-
-        .dot {
-          border: none;
-          width: 10px;
-          height: 10px;
-          background: #c5c5c5;
-          border-radius: 50%;
-          margin: 0 5px;
-          padding: 5px;
-          cursor: pointer;
-        }
-
-        .dot:focus {
-          outline: none;
-        }
-
-        .dot.active {
-          background: #000;
-        }
-      `}</style>
       <div className="navigation-wrapper">
         <div ref={sliderRef} className="keen-slider">
           <div className="keen-slider__slide number-slide1">1</div>
