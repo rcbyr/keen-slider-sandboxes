861cdcebec82ae13d638c0b3ccb87652bbf0a6ab
Eric Beyer <contact@ericbeyer.de>
2021-11-27 00:24:47 +0100

    add background-rotation

--- misc/background-rotation/react-typescript/src/App.tsx
+++ misc/background-rotation/react-typescript/src/App.tsx
@@ -4,17 +4,36 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 
 export default function App() {
-  const [ref] = useKeenSlider<HTMLDivElement>({
+  const [rotation, setRotation] = React.useState({})
+  const [lastProgress, setLastProgress] = React.useState(0)
+
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    slides: 2,
+    detailsChanged(s) {
+      const progress = s.track.details.progress
+      const delta = lastProgress - progress
+      setLastProgress(progress)
+      setRotation(delta * 360)
+    },
     loop: true,
   })
+
   return (
-    <div ref={ref} className="keen-slider">
-      <div className="keen-slider__slide number-slide1">1</div>
-      <div className="keen-slider__slide number-slide2">2</div>
-      <div className="keen-slider__slide number-slide3">3</div>
-      <div className="keen-slider__slide number-slide4">4</div>
-      <div className="keen-slider__slide number-slide5">5</div>
-      <div className="keen-slider__slide number-slide6">6</div>
+    <div
+      style={{
+        backgroundImage: `linear-gradient(${rotation}deg, black 0px, black 50%, white 50%, white 100%)`,
+      }}
+      className="background-rotation"
+      ref={sliderRef}
+    >
+      <div
+        className="background-rotation__inner"
+        style={{
+          backgroundImage: `linear-gradient(${rotation}deg, white 0px, white 50%, black 50%, black 100%)`,
+        }}
+      >
+        <span>keen-slider</span>
+      </div>
     </div>
   )
 }
