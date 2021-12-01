732ddc33465aba2d410e730a3124c5845fd28647
Eric Beyer <contact@ericbeyer.de>
2021-12-01 21:14:16 +0100

    update versions

---
+++ navigation/thumbnails/react-typescript/src/App.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+import "./styles.css"
+import {
+  useKeenSlider,
+  KeenSliderPlugin,
+  KeenSliderInstance,
+} from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+
+function ThumbnailPlugin(
+  mainRef: MutableRefObject<KeenSliderInstance | null>
+): KeenSliderPlugin {
+  return (slider) => {
+    function removeActive() {
+      slider.slides.forEach((slide) => {
+        slide.classList.remove("active")
+      })
+    }
+    function addActive(idx: number) {
+      slider.slides[idx].classList.add("active")
+    }
+
+    function addClickEvents() {
+      slider.slides.forEach((slide, idx) => {
+        slide.addEventListener("click", () => {
+          if (mainRef.current) mainRef.current.moveToIdx(idx)
+        })
+      })
+    }
+
+    slider.on("created", () => {
+      if (!mainRef.current) return
+      addActive(slider.track.details.rel)
+      addClickEvents()
+      mainRef.current.on("animationStarted", (main) => {
+        removeActive()
+        const next = main.animator.targetIdx || 0
+        addActive(next)
+        slider.moveToIdx(next)
+      })
+    })
+  }
+}
+
+const Component = () => {
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+  })
+  const [thumbnailRef] = useKeenSlider<HTMLDivElement>(
+    {
+      initial: 0,
+      slides: {
+        perView: 4,
+        spacing: 10,
+      },
+    },
+    [ThumbnailPlugin(instanceRef)]
+  )
+
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+
+      <div ref={thumbnailRef} className="keen-slider thumbnail">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+    </>
+  )
+}
---
+++ navigation/thumbnails/react/src/App.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import "./styles.css"
+
+function ThumbnailPlugin(mainRef) {
+  return (slider) => {
+    function removeActive() {
+      slider.slides.forEach((slide) => {
+        slide.classList.remove("active")
+      })
+    }
+    function addActive(idx) {
+      slider.slides[idx].classList.add("active")
+    }
+
+    function addClickEvents() {
+      slider.slides.forEach((slide, idx) => {
+        slide.addEventListener("click", () => {
+          if (mainRef.current) mainRef.current.moveToIdx(idx)
+        })
+      })
+    }
+
+    slider.on("created", () => {
+      if (!mainRef.current) return
+      addActive(slider.track.details.rel)
+      addClickEvents()
+      mainRef.current.on("animationStarted", (main) => {
+        removeActive()
+        const next = main.animator.targetIdx || 0
+        addActive(next)
+        slider.moveToIdx(next)
+      })
+    })
+  }
+}
+
+const Component = () => {
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+  })
+  const [thumbnailRef] = useKeenSlider(
+    {
+      initial: 0,
+      slides: {
+        perView: 4,
+        spacing: 10,
+      },
+    },
+    [ThumbnailPlugin(instanceRef)]
+  )
+
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+
+      <div ref={thumbnailRef} className="keen-slider thumbnail">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+    </>
+  )
+}
