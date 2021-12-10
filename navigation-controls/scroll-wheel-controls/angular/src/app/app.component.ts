3c572507d64f919cd6045b00dce8a36dd503ab8e
Eric Beyer <contact@ericbeyer.de>
2021-12-10 12:38:27 +0100

    add angular example

---
+++ navigation-controls/scroll-wheel-controls/angular/src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, ElementRef, ViewChild } from "@angular/core"
+import KeenSlider from "keen-slider"
+
+const WheelControls = (slider) => {
+  let touchTimeout
+  let position
+  let wheelActive
+
+  function dispatch(e, name) {
+    position.x -= e.deltaX
+    position.y -= e.deltaY
+    slider.container.dispatchEvent(
+      new CustomEvent(name, {
+        detail: {
+          x: position.x,
+          y: position.y,
+        },
+      })
+    )
+  }
+
+  function wheelStart(e) {
+    position = {
+      x: e.pageX,
+      y: e.pageY,
+    }
+    dispatch(e, "ksDragStart")
+  }
+
+  function wheel(e) {
+    dispatch(e, "ksDrag")
+  }
+
+  function wheelEnd(e) {
+    dispatch(e, "ksDragEnd")
+  }
+
+  function eventWheel(e) {
+    e.preventDefault()
+    if (!wheelActive) {
+      wheelStart(e)
+      wheelActive = true
+    }
+    wheel(e)
+    clearTimeout(touchTimeout)
+    touchTimeout = setTimeout(() => {
+      wheelActive = false
+      wheelEnd(e)
+    }, 50)
+  }
+
+  slider.on("created", () => {
+    slider.container.addEventListener("wheel", eventWheel, {
+      passive: false,
+    })
+  })
+}
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "../../node_modules/keen-slider/keen-slider.min.css",
+    "./app.component.css",
+  ],
+})
+export class AppComponent {
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+
+  slider: KeenSliderInstance = null
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(
+      this.sliderRef.nativeElement,
+      {
+        loop: false,
+        rubberband: false,
+        vertical: true,
+      },
+      [WheelControls]
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
+}
