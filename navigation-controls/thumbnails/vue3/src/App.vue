b270824079aedf45430abfdc0ca4215dc66bd6ed
Eric Beyer <contact@ericbeyer.de>
2021-12-10 23:41:09 +0100

    update versions

---
+++ navigation-controls/thumbnails/vue3/src/App.vue
@@ -0,0 +1,153 @@
+<template>
+<div>
+    <div ref="container" class="keen-slider">
+      <div class="keen-slider__slide number-slide1">1</div>
+      <div class="keen-slider__slide number-slide2">2</div>
+      <div class="keen-slider__slide number-slide3">3</div>
+      <div class="keen-slider__slide number-slide4">4</div>
+      <div class="keen-slider__slide number-slide5">5</div>
+      <div class="keen-slider__slide number-slide6">6</div>
+    </div> 
+    <div ref="thumbnail" class="keen-slider thumbnail">
+      <div class="keen-slider__slide number-slide1">1</div>
+      <div class="keen-slider__slide number-slide2">2</div>
+      <div class="keen-slider__slide number-slide3">3</div>
+      <div class="keen-slider__slide number-slide4">4</div>
+      <div class="keen-slider__slide number-slide5">5</div>
+      <div class="keen-slider__slide number-slide6">6</div>
+    </div>
+</div>
+</template>
+
+<script>
+import { useKeenSlider } from 'keen-slider/vue.es'
+import 'keen-slider/keen-slider.min.css'
+
+
+function ThumbnailPlugin(main) {
+  return (slider) => {
+    function removeActive() {
+      slider.slides.forEach((slide) => {
+        slide.classList.remove("active")
+      })
+    }
+    function addActive(idx) {
+      slider.slides[idx].classList.add("active")
+    }
+
+    function addClickEvents() {
+      slider.slides.forEach((slide, idx) => {
+        slide.addEventListener("click", () => {
+          main.moveToIdx(idx)
+        })
+      })
+    }
+
+    slider.on("created", () => {
+      addActive(slider.track.details.rel)
+      addClickEvents()
+      main.on("animationStarted", (main) => {
+        removeActive()
+        const next = main.animator.targetIdx || 0
+        addActive(next)
+        slider.moveToIdx(next)
+      })
+    })
+  }
+}
+
+export default {
+  setup(){
+    const [container] = useKeenSlider()
+    const [thumbnail] = useKeenSlider(
+      {
+        initial: 0,
+        slides: {
+          perView: 4,
+          spacing: 10,
+        },
+      },
+      [ThumbnailPlugin(this.slider)])
+    return { container, thumbnail }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Inter', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+[class^="number-slide"],
+[class*=" number-slide"] {
+  background: grey;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 50px;
+  color: #fff;
+  font-weight: 500;
+  height: 300px;
+  max-height: 100vh;
+}
+
+.number-slide1 {
+  background: rgb(64, 175, 255);
+  background: linear-gradient(
+    128deg,
+    rgba(64, 175, 255, 1) 0%,
+    rgba(63, 97, 255, 1) 100%
+  );
+}
+
+.number-slide2 {
+  background: rgb(255, 75, 64);
+  background: linear-gradient(
+    128deg,
+    rgba(255, 154, 63, 1) 0%,
+    rgba(255, 75, 64, 1) 100%
+  );
+}
+
+.number-slide3 {
+  background: rgb(182, 255, 64);
+  background: linear-gradient(
+    128deg,
+    rgba(182, 255, 64, 1) 0%,
+    rgba(63, 255, 71, 1) 100%
+  );
+  background: linear-gradient(
+    128deg,
+    rgba(189, 255, 83, 1) 0%,
+    rgba(43, 250, 82, 1) 100%
+  );
+}
+
+.number-slide4 {
+  background: rgb(64, 255, 242);
+  background: linear-gradient(
+    128deg,
+    rgba(64, 255, 242, 1) 0%,
+    rgba(63, 188, 255, 1) 100%
+  );
+}
+
+.number-slide5 {
+  background: rgb(255, 64, 156);
+  background: linear-gradient(
+    128deg,
+    rgba(255, 64, 156, 1) 0%,
+    rgba(255, 63, 63, 1) 100%
+  );
+}
+.number-slide6 {
+  background: rgb(64, 76, 255);
+  background: linear-gradient(
+    128deg,
+    rgba(64, 76, 255, 1) 0%,
+    rgba(174, 63, 255, 1) 100%
+  );
+}
+</style>
