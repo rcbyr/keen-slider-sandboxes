f1b8b36e313cf31cfc01e97b25f801ee37610958
Eric Beyer <contact@ericbeyer.de>
2021-12-12 21:24:40 +0100

    add new renderMode

--- misc/draggable-card/react-typescript/src/App.tsx
+++ misc/draggable-card/react-typescript/src/App.tsx
@@ -6,49 +6,41 @@ import "keen-slider/keen-slider.min.css"
 export default function App() {
   const [scrolling, setScrolling] = useState(true)
   const smallHeight = React.useRef(0)
-  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>(
-    {
-      initial: 2,
-      vertical: true,
-      rubberband: true,
-      slides: (size, elements) => {
-        smallHeight.current = elements[0].getBoundingClientRect().height
-        const mediumHeight = elements[1].getBoundingClientRect().height
+  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
+    initial: 2,
+    vertical: true,
+    rubberband: true,
+    slides: (size, elements) => {
+      smallHeight.current = elements[0].getBoundingClientRect().height
+      const mediumHeight = elements[1].getBoundingClientRect().height
 
-        size -= smallHeight.current
-        console.log(size, smallHeight.current, elements)
+      size -= smallHeight.current
+      console.log(size, smallHeight.current, elements)
 
-        return [
-          { size: smallHeight.current / size, origin: 0 },
-          { size: mediumHeight / size, origin: 0 },
-          {
-            size: (size - smallHeight.current - mediumHeight) / size,
-            origin: -(size - smallHeight.current - mediumHeight) / size,
-          },
-        ]
-      },
-      detailsChanged(slider) {
-        const y =
-          (slider.size - smallHeight.current) *
-          slider.track.details.length *
-          slider.track.details.progress *
-          -1
-        slider.container.style.transform = `translate3d(0, ${Math.round(
-          y
-        )}px, 0)`
-      },
-      slideChanged(slider) {
-        setScrolling(slider.track.details.rel === 2)
-        // desktop fix
-        slider.container
-          .querySelector(".card__inner__scrolling")
-          ?.scrollTo(0, 0)
-      },
+      return [
+        { size: smallHeight.current / size, origin: 0 },
+        { size: mediumHeight / size, origin: 0 },
+        {
+          size: (size - smallHeight.current - mediumHeight) / size,
+          origin: -(size - smallHeight.current - mediumHeight) / size,
+        },
+      ]
     },
-    {
-      renderer: false,
-    }
-  )
+    detailsChanged(slider) {
+      const y =
+        (slider.size - smallHeight.current) *
+        slider.track.details.length *
+        slider.track.details.progress *
+        -1
+      slider.container.style.transform = `translate3d(0, ${Math.round(y)}px, 0)`
+    },
+    renderMode: "custom",
+    slideChanged(slider) {
+      setScrolling(slider.track.details.rel === 2)
+      // desktop fix
+      slider.container.querySelector(".card__inner__scrolling")?.scrollTo(0, 0)
+    },
+  })
 
   return (
     <div className="card">
