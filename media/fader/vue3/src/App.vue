5973c824135db68de685650f6b98dd20eabda191
Eric Beyer <contact@ericbeyer.de>
2021-12-12 12:17:17 +0100

    fix example

--- media/fader/vue3/src/App.vue
+++ media/fader/vue3/src/App.vue
@@ -12,24 +12,31 @@
 </template>
 
 <script>
+import {  ref } from 'vue'
 import { useKeenSlider } from 'keen-slider/vue.es'
 import 'keen-slider/keen-slider.min.css'
 
 
 export default {
   setup(){
+    const images = [
+      "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+      "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+      "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+      "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+    ];
+    const opacities = ref([])
     const [container] = useKeenSlider({
-      slides: this.images.length,
+      slides: images.length,
       loop: true,
       defaultAnimation: {
         duration: 3000
       },
       detailsChanged: (s) => {
-        const opacities = s.track.details.slides.map((slide) => slide.portion)
-        this.setOpacities(opacities)
+        opacities = s.track.details.slides.map((slide) => slide.portion)
       },
     })
-    return { container }
+    return { container, images, opacities }
   }
 }
 </script>
