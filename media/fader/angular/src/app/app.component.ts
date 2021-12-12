a94f585672de088e5b9152891bb1bb7246764942
Eric Beyer <contact@ericbeyer.de>
2021-12-12 12:09:35 +0100

    add example

---
+++ media/fader/angular/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, ElementRef, ViewChild } from "@angular/core"
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "../../node_modules/keen-slider/keen-slider.min.css",
+    "./app.component.css",
+  ],
+})
+export class AppComponent {
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+  images: String[] = [
+    "https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+    "https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+    "https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+    "https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80",
+  ]
+  opacities: number[] = []
+  slider: KeenSliderInstance = null
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        slides: this.images.length,
+        loop: true,
+        defaultAnimation: {
+          duration: 3000,
+        },
+        detailsChanged: (s) => {
+          const opacities = s.track.details.slides.map((slide) => slide.portion)
+          this.setOpacities(opacities)
+        },
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
+}
