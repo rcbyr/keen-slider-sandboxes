7b5dcdd9ad0194faf3fd67bce5e81f1f6a68b6f3
Eric Beyer <contact@ericbeyer.de>
2021-12-14 07:59:27 +0100

    add examples

---
+++ ios-like-picker/time-loop/vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    :style="{
+      height: '200px',
+      padding: '50px',
+      display: 'flex',
+      'justify-content': 'center',
+      background: '#000',
+    }"
+  >
+    <div :style="{ width: '70px' }">
+      <Wheel :initIdx="1" :length="24" :width="23" :loop="true" />
+    </div>
+    <div :style="{ width: '70px' }">
+      <Wheel
+        :initIdx="35"
+        :length="60"
+        :width="23"
+        perspective="left"
+        :loop="true"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Wheel from "./Wheel"
+export default {
+  components: {
+    Wheel,
+  },
+  name: "Timepicker",
+}
+</script>
+
+<style></style>
